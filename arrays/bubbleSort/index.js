17ea8f4c9144ff24fd73c1c09cb928ed76bc0731
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2023-08-10 09:28:21 -0500

    fes dsa problems

---
+++ arrays/bubbleSort/index.js
@@ -0,0 +1,37 @@
+/*  Bubble Sort
+
+    Sort the 'nums' array using bubble sort
+
+    @example
+    bubbleSort([3,2,1]) -> [1,2,3]
+    bubbleSort([0,12,8]) -> [0,8,12]
+    bubbleSort([3,4,0,2]) -> [0,2,3,4]
+
+*/
+
+const bubbleSort = (nums) => {
+
+    let swap = true
+
+    for(let i = 0; i < nums.length; i++){
+
+        if(!swap)
+            break;
+
+        for(let j = 1; j < nums.length; j++){
+            
+            if(nums[j - 1] < nums[j]){
+                let temp = nums[j]
+                nums[j] = nums[j - 1]
+                nums[j - 1] = temp
+                swap = false
+            }
+        }
+    }
+
+    console.log(nums)
+    return nums
+
+}
+
+module.exports = bubbleSort;
